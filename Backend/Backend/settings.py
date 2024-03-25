53db7ac289252e746f71b497ce46914f9a078af0
Nataniel Chmielarz <129111960+NatanielChmielarz@users.noreply.github.com>
2024-03-25 13:18:33 +0100

    commit

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -92,12 +92,15 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("POSTGRES_DATABASE"),
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("POSTGRES_HOST"),
         'PORT': os.getenv("POSTGRES_DB_PORT"),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
